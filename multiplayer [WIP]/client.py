7d090acc7168120a337c00ef85aa2b7998ec261b
ItsbaileyX3525 <80989969+ItsbaileyX3525@users.noreply.github.com>
2023-02-06 07:35:42 +0000

    its working!

--- multiplayer [WIP]/client.py
+++ multiplayer [WIP]/client.py
@@ -1,44 +1,20 @@
-from ursina import *
-import random as ra
-from ursina.prefabs.first_person_controller import FirstPersonController
 from ursinanetworking import *
-from time import sleep
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+import threading
+import time
+from shared import *
 
-App = Ursina()
-Client = UrsinaNetworkingClient("localhost", 25565)
-Easy = EasyUrsinaNetworkingClient(Client)
+app = Ursina()
 window.borderless = False
 
-Players = {}
-PlayersTargetPos = {}
-SelfId = -1
+Client = UrsinaNetworkingClient("localhost", 25565)
+Blabla = ReplicatedClEventsHandler(Client)
 
-@Client.event
-def GetId(Id):
-    global SelfId
-    SelfId = Id
-    print(f"My ID is : {SelfId}")
-
-@Easy.event
-def onReplicatedVariableUpdated(variable):
-    PlayersTargetPos[variable.name] = variable.content["position"]
-
-class Player(FirstPersonController):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.mouse_sensitivity = (155, 155)
-Ply = Player()
 
 def update():
+    Client.process_net_events()
+    Blabla.replicated_update()
 
-    if Ply.position[1] < -5:
-        Ply.position = (ra.randrange(0, 15), 10, ra.randrange(0, 15))
-
-    for p in Players:
-        try:
-            Players[p].position += (Vec3(PlayersTargetPos[p]) - Players[p].position) / 25
-        except Exception as e: print(e)
-    
-    Easy.process_net_events()
-
-App.run()
+player = FirstPersonController()
+app.run()
