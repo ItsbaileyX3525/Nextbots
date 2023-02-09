1a867b06bc9ceecb705781513e51541dd11d872a
ItsbaileyX3525 <80989969+ItsbaileyX3525@users.noreply.github.com>
2023-02-09 22:44:19 +0000

    Add files via upload

--- maincode.py
+++ maincode.py
@@ -1,3 +1,24 @@
+"""
+Update log:
+
+Nextbots V7.0:
+- Added player animations
+- Fixed bug where nextbots would stop turning after chasing you once
+- Added random nextbot spawning (Really not the best system ATM)
+- Fixed bug where every single time second nextbot would be Dr lively
+- Added comments to the code so ppl can understand cause im nice and ppl make me do so much work :( (~20% commented)
+- More to come!
+- Removed the random ass "dist" variable
+- Mr. Tumble removed for crashing the game every time he caught you
+
+Known bugs:
+- Player animations dont work if you hold down 'w' and then hold down 's' and release 's' it'll keep you idle does this with every combination
+- Sometimes Armstrongs texture can bug out a little if hes classed as a second nextbot
+- On occasion the nextbot chase sound effect will play for a split second upon clicking the start game button
+- Jumpscare bg are transparent (cba making them not rn)
+"""
+
+
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 from ursina.prefabs.health_bar import HealthBar
@@ -6,8 +27,8 @@ from direct.stdpy import thread
 from direct.actor.Actor import Actor
 from pandac.PandaModules import TransparencyAttrib
 from ursina.prefabs.health_bar import HealthBar
-from panda3d.core import Texture, AnimControlCollection
-from direct.interval.IntervalGlobal import LerpHprInterval
+from panda3d.core import Texture
+from direct.interval.IntervalGlobal import LerpHprInterval, ActorInterval
 
 window.title="Nextbots"
 
@@ -15,9 +36,55 @@ window.fullscreen=False
 window.borderless=False
 window.icon="assets/misc/papyrus.ico"
 
-dist = 0
+class Character(Entity): # Player model, attaches to the Harlod (FPC)
+    def __init__(self):
+        super().__init__()
+        self.actor=Actor("player.gltf")
+        self.actor.reparentTo(Harlod)
+        self.actor.setScale(0.018)
+        self.actor.setHpr(180,0,0)
+        x,y,z=self.actor.getPos()
+        self.actor.setPos(x,.9,1)
+        self.idle=ActorInterval(self.actor, "idle")
+        self.run=ActorInterval(self.actor, "run")
+        self.runbackward=ActorInterval(self.actor, "run backwards")
+        self.walk=ActorInterval(self.actor, "walk")
+        self.walkbackward=ActorInterval(self.actor, "walk backwards")
+        self.straferight=ActorInterval(self.actor, "strafe right")
+        self.strafeleft=ActorInterval(self.actor, "strafe left")
+        self.idle.loop()
 
-class Nextbot(Entity):
+    def input(self, key):
+        if key == 'w' and not held_keys['shift']:
+            self.walk.loop()
+        elif key == 'w' and  held_keys['shift']:
+            self.run.loop()
+        elif held_keys['w'] and key=='shift':
+            self.run.loop()
+        elif key == 's' and not held_keys['shift']:
+            self.walkbackward.loop()
+        elif key=='s'  and held_keys['shift']:
+            self.runbackward.loop()
+        elif held_keys['s'] and key=='shift':
+            self.runbackward.loop()
+        elif key == 'a':
+            self.strafeleft.loop()
+        elif key == 'd':
+            self.straferight.loop()
+        elif key == 'd up':
+            self.idle.loop()
+        elif key == 'a up':
+            self.idle.loop()
+        elif key == 's up':
+            self.idle.loop()
+        elif key == 'w up':
+            self.idle.loop()
+        elif key=='shift up' and held_keys['w']:
+            self.walk.loop()
+        elif key=='shift up' and held_keys['s']:
+            self.walkbackward.loop()
+
+class Nextbot(Entity): #Nextbot class for creating new nextbots
     def __init__(self, texture, chase_sound, death_sound, death_texture, **kwargs):
         super().__init__(parent=sus, model='quad', texture=texture, scale_y=3,scale_x=3, collider='box', y=2, double_sided=True, **kwargs)
         self.chase_sound = chase_sound
@@ -25,22 +92,21 @@ class Nextbot(Entity):
         self.death_texture = death_texture
         self.move=None
         self.in_range=None
-        self.max_distance = 30
-        self.chase_sound.play()
+        self.dist=0
+        self.max_distance = 30 #How far away audio plays from the nextbot
         self.Nextbot_rotate()
-
+        self.chase_sound.play()
 
     def update(self):
-        global dist
-        dist = distance(Harlod.position, self.position)
-        volume = max(1 - dist / self.max_distance, 0)
+        self.dist = distance(Harlod.position, self.position)
+        volume = max(1 - self.dist / self.max_distance, 0)
         self.chase_sound.volume = volume
-        if dist > 1.2 and dist < 18:
+        if self.dist > 1.2 and self.dist < 18:
             self.move=False
             self.in_range=True
             self.look_at_2d(Harlod.position, 'y')
             self.position += self.forward * time.dt * 10
-        elif dist < 1.2:
+        elif self.dist < 1.2:
             if not jumpscare.playing:
                 global nomove
                 jumpscare.play()
@@ -58,71 +124,29 @@ class Nextbot(Entity):
                     deathL = editor_camera.add_script(SmoothFollow(target=Harlod, offset=(0,2,-10)))
                     playerdeath = True
                 invoke(playerdeath, delay=2)
+        if self.dist > 18:
+            self.in_range=False
         if self.move:
             self.position += self.forward * time.dt * 8
 
     def Nextbot_move(self):
-        if dist > 18:
+        if self.dist > 18:
             self.move=True
         invoke(self.Nextbot_rotate, delay=ra.uniform(1,3))
 
     def Nextbot_rotate(self):
         self.move=False
         delay = ra.uniform(1, 3)
-        if dist > 18:
+        if self.dist > 18:
             if not self.in_range:
                 rotate_interval = LerpHprInterval(self, delay, (ra.uniform(0,260),0,0))
                 rotate_interval.start()
         invoke(self.Nextbot_move, delay=delay)
 
-class LoadingWheel(Entity):
-    def __init__(self, **kwargs):
-        super().__init__()
-        self.parent = camera.ui
-        self.point = Entity(parent=self, model=Circle(24, mode='point', thickness=.03), color=color.light_gray, y=.75, scale=2, texture='circle')
-        self.point2 = Entity(parent=self, model=Circle(12, mode='point', thickness=.03), color=color.light_gray, y=.75, scale=1, texture='circle')
-
-        self.scale = .025
-        self.text_entity = Text(world_parent=self, text='loading...', origin=(0,1.5), color=color.light_gray)
-        self.y = -.25
-
-        self.bg = Entity(parent=self, model='quad', scale_x=camera.aspect_ratio, color=color.black, z=1)
-        self.bg.scale *= 400
-
-        for key, value in kwargs.items():
-            setattr(self, key ,value)
-
-
-    def update(self):
-        self.point.rotation_y += 5
-        self.point2.rotation_y += 3
 
 
 app=Ursina()
 
-info_text = Text('''Press space to start loading textures''', origin=(0,0), color=color.white,z=-100)
-loading_screen = LoadingWheel(enabled=False)
-def load_textures():
-    global load_bg
-    textures_to_load = ['assets/textures/obunga', 'assets/textures/andrew', 'assets/textures/Sans', 'assets/textures/saddydaddy', 'assets/textures/angy munci', 'assets/textures/phonk.gif'] * 50
-    bar = HealthBar(max_value=len(textures_to_load), value=0, position=(-.5,-.35,-2), scale_x=1, animation_duration=0, world_parent=loading_screen, bar_color=color.gray)
-    for i, t in enumerate(textures_to_load):
-        load_texture(t)
-        bar.value = i+1
-    # destroy(bar, delay=.01)
-    loading_screen.enabled = False
-
-
-loading_screen.enabled = True
-info_text.enabled = False
-t = time.time()
-
-try:
-    thread.start_new_thread(function=load_textures, args='')
-except Exception as e:
-    print('error starting thread', e)
-
-print('---', time.time()-t)
 
 editor_camera = EditorCamera(enabled=False)
 sus=Entity()
@@ -135,13 +159,12 @@ health_bar_2.animation_duration = 0
 
 Nextbotted1=None
 Nextbotted2=None
-nextbot1=2
-nextbot2=ra.randint(1,5)
 phonk_texture=None
 phonk_texture1=None
-
+armstrong_texture=None
+armstrong_texture1=None
 def game_begin():
-    global load_bg,start,health_bar_2,health_bar_1,Nextbotted1,Nextbotted2,phonk_texture,phonk_texture1
+    global load_bg,start,health_bar_2,health_bar_1,Nextbotted1,Nextbotted2,phonk_texture,phonk_texture1,nextbot1_1,nextbot1_2,nextbot1_3,nextbot1_4,nextbot1_5,nextbot1_6,armstrong_texture,armstrong_texture1
     Harlod.speed=8
     ButtonClick.play()
     load_bg.enabled=False
@@ -156,8 +179,15 @@ def game_begin():
     nextbot1_3=False
     nextbot1_4=False
     nextbot1_5=False
+    nextbot1_6=False
+    nextbots1()
+    nextbots2()
+    Entity(update=gif_applier)
     #nextbot3=ra.uniform(1,10) < third nextbot when theres gonna be loads more nextbots
     #Random nextbot spawner using the ra.randint from before
+def nextbots1():
+    global nextbot1,nextbot2,Nextbotted1,Nextbotted2,phonk_texture,phonk_texture1,armstrong_texture,armstrong_texture1
+    nextbot1=6
     if nextbot1==1:
         nextbot1_1=True
         Nextbotted1 = Nextbot(texture='assets/textures/obunga.png', chase_sound=obungachase, death_sound=death, death_texture='assets/textures/obunga.png',x=ra.uniform(-80,80),z=ra.uniform(-80,80))
@@ -166,8 +196,6 @@ def game_begin():
         phonk_texture = Animation('assets/textures/phonk.gif')
         phonk_texture1 = Animation('assets/textures/phonk.gif')
         Nextbotted1 = Nextbot(texture='assets/textures/phonk.png', chase_sound=PhonkChase, death_sound=death, death_texture='assets/textures/phonk.gif',x=ra.uniform(-80,80),z=ra.uniform(-80,80))
-        Entity(update=gif_applier)
-
     if nextbot1==3:
         nextbot1_3=True
         Nextbotted1=Nextbot(texture='assets/textures/saddydaddy.png', chase_sound=AutismCreatureChase, death_sound=death, death_texture='assets/textures/saddydaddy.png',x=ra.uniform(-80,80),z=ra.uniform(-80,80))
@@ -177,37 +205,50 @@ def game_begin():
     if nextbot1==5:
         nextbot1_5=True
         Nextbotted1=Nextbot(texture='assets/textures/angy munci.png', chase_sound=muncichase, death_sound=death, death_texture='assets/textures/angy munci.png',x=ra.uniform(-80,80),z=ra.uniform(-80,80))
+    if nextbot1==6:
+        armstrong_texture = Animation('assets/textures/armstrong.gif')
+        armstrong_texture1 = Animation('assets/textures/armstrong.gif')
+        nextbot1_6=True
+        Nextbotted1=Nextbot(texture='assets/textures/phonk.png', chase_sound=armstrongchase, death_sound=death, death_texture='assets/textures/armstrong.gif',x=ra.uniform(-80,80),z=ra.uniform(-80,80))
+def nextbots2():
+    global nextbot1,nextbot2,nextbot1_1,nextbot1_2,nextbot1_3,nextbot1_4,nextbot1_5,nextbot1_6,Nextbotted1,Nextbotted2,phonk_texture,phonk_texture1,armstrong_texture1,armstrong_texture
+    nextbot2=ra.randint(1,6)
     if nextbot2==1:
         if nextbot1_1:
-            pass
+            nextbots2()
         else:
             Nextbotted2 = Nextbot(texture='assets/textures/obunga.png', chase_sound=obungachase, death_sound=death, death_texture='assets/textures/obunga.png',x=ra.uniform(-80,80),z=ra.uniform(-80,80))
     if nextbot2==2:
         if nextbot1_2:
-            pass
+            nextbots2()
         else:
             phonk_texture = Animation('assets/textures/phonk.gif')
             phonk_texture1 = Animation('assets/textures/phonk.gif')
             Nextbotted2 = Nextbot(texture='assets/textures/phonk.png', chase_sound=PhonkChase, death_sound=death, death_texture='assets/textures/phonk.gif',x=ra.uniform(-80,80),z=ra.uniform(-80,80))
-            Entity(update=gif_applier)
     if nextbot2==3:
         if nextbot1_3:
-            pass
+            nextbots2()
         else:
             Nextbotted2=Nextbot(texture='assets/textures/saddydaddy.png', chase_sound=AutismCreatureChase, death_sound=death, death_texture='assets/textures/saddydaddy.png',x=ra.uniform(20,80),z=ra.uniform(20,80))
     if nextbot2==4:
         if nextbot1_4:
-            pass
+            nextbots2()
         else:
             Nextbotted2=Nextbot(texture='assets/textures/andrew.png', chase_sound=tateyChase, death_sound=death, death_texture='assets/textures/andrew.png',x=ra.uniform(20,80),z=ra.uniform(20,80))
     if nextbot2==5:
         if nextbot1_5:
-            pass
+            nextbots2()
         else:
             Nextbotted2=Nextbot(texture='assets/textures/angy munci.png', chase_sound=muncichase, death_sound=death, death_texture='assets/textures/angy munci.png',x=ra.uniform(-80,80),z=ra.uniform(-80,80))
-    
+    if nextbot2==6:
+        if nextbot1_6:
+            nextbots2()
+        else:
+            armstrong_texture = Animation('assets/textures/armstrong.gif')
+            armstrong_texture1 = Animation('assets/textures/armstrong.gif')
+            Nextbotted2=Nextbot(texture='assets/textures/phonk.png', chase_sound=armstrongchase, death_sound=death, death_texture='assets/textures/armstrong.gif',x=ra.uniform(-80,80),z=ra.uniform(-80,80))
 
-def gif_applier():
+def gif_applier(): #Makes the double-sided gif work for the nextbots
     global nextbot1,nextbot2
     if nextbot1==2:
         phonk_texture.position = Nextbotted1.position
@@ -223,7 +264,22 @@ def gif_applier():
         phonk_texture1.position = Nextbotted2.position
         phonk_texture1.scale = Nextbotted2.scale
         phonk_texture1.rotation=Nextbotted2.rotation_x + 180
+    if nextbot1==6:
+        armstrong_texture.position = Nextbotted1.position
+        armstrong_texture.scale = Nextbotted1.scale
+        armstrong_texture.rotation=Nextbotted1.rotation
+        armstrong_texture1.position = Nextbotted1.position
+        armstrong_texture1.scale = Nextbotted1.scale
+        armstrong_texture1.rotation_y=Nextbotted1.rotation_y - 180
+    elif nextbot2==6:
+        armstrong_texture.position = Nextbotted2.position
+        armstrong_texture.scale = Nextbotted2.scale
+        armstrong_texture.rotation=Nextbotted2.rotation
+        armstrong_texture1.position = Nextbotted2.position
+        armstrong_texture1.scale = Nextbotted2.scale
+        armstrong_texture1.rotation=Nextbotted2.rotation_x + 180
 #Audios
+armstrongchase=Audio('assets/audio/armstrong',loop=True,autoplay=False)
 muncichase=Audio('assets/audio/vacent1',loop=True,autoplay=False,volume=.8)
 obungachase=Audio('assets/audio/prowler',autoplay=False,loop=True)
 sanschase=Audio('assets/audio/megalovania',autoplay=False,loop=True)
@@ -234,7 +290,7 @@ AutismCreatureChase=Audio('assets/audio/saddyclose',autoplay=False,loop=True)
 death=Audio('assets/audio/death',autoplay=False,loop=False,volume=2)
 jumpscare=Audio('assets/audio/jumpscare',autoplay=False,loop=False)
 
-ButtonClick=Audio('assets/audio/button-click',autoplay=False,loop=False)
+ButtonClick=Audio('assets/audi+o/button-click',autoplay=False,loop=False)
 
 
 #main menu
@@ -247,18 +303,21 @@ window.color = color.white
 Audio('welcome')
 playerdeath=False
 
-from fur_shader import *
+from fur_shader import Fur
+#CakedGround = Entity(model="cube",collider='box', color=color.white, texture="grass",scale=(1000,0.1,1000),texture_scale=(31.6227766017,1,31.6227766017))
 ground = Entity(model='plane', scale=1000, texture='grass', texture_scale=(31.6227766017,31.6227766017), collider='box')
-#grass = Fur(entity=ground, scale=800, layers=3, layerSize=0.005, shadow=20)
+#grass = Fur(entity=cube, scale=30000, layers=3, layerSize=0.005, shadow=20)
 
 window.exit_button.visible=False
 window.fps_counter.enabled=True
 
 #Harlod/player stuff
-Harlod=FirstPersonController(model='cube',collider='sphere', z=-10, color=color.orange,origin_y=0, speed=0)
+Harlod=FirstPersonController()
+HarlodModel=Character()
 mouse.locked=False
 Harlod.walkSpeed=6
 Harlod.runSpeed=17
+Harlod.jogSpeed=12
 healthtext=Text(text=f'{health_bar_2.value}/{health_bar_2.max_value}',x=-.5,y=-.4)
 healthtext1=Text(text='Health:',x=-.8,y=-.4)
 healthbox=Entity(alpha=.5,color=color.yellow)
@@ -268,7 +327,7 @@ def healthupdate():
 Entity(update=healthupdate)
     
 #crorbar innit
-Crowbar1=Actor("crowbar.gltf")
+Crowbar1=Actor("crowbar9.gltf")
 Crowbar1.reparentTo(camera.ui)
 #Crowbar1.setScale(0.023)
 Crowbar1.setScale(0.000000000000000000000000000000000000000000000023)
@@ -316,7 +375,6 @@ def respawn_screen():
 
 playerdeath=False
 
-
 Sky()
 
 
@@ -326,14 +384,14 @@ def input(key):
             window.fullscreen=False
         else:
             window.fullscreen=True
-
-
 seq1=False
 
 health_regen_timer = 0
 
 def update():
     global playerdeath, seq1, health_regen_timer,respawn,Nextbotted1
+    round(health_bar_1.value, 1)
+    print(Harlod.speed)
     if playerdeath == True:
         if not seq1:
             Crowbar1.setTransparency(TransparencyAttrib.M_alpha)
@@ -342,11 +400,18 @@ def update():
         if Harlod.y==Nextbotted1.y:
             playerdeath = False
             Harlod.position-=Harlod.position
-    if held_keys['shift'] and held_keys['w'] and not nomove:
+    if held_keys['shift'] and held_keys['w'] and not nomove and not held_keys['s']:
         if health_bar_1.value == 0:
             Harlod.speed = Harlod.walkSpeed
         else:
             Harlod.speed = Harlod.runSpeed
+            health_bar_1.value -= 0.25
+            health_regen_timer = 0
+    elif held_keys['shift'] and held_keys['s'] and not nomove and not held_keys['w']:
+        if health_bar_1.value == 0:
+            Harlod.speed = Harlod.walkSpeed
+        else:
+            Harlod.speed = Harlod.jogSpeed
             health_bar_1.value -= 0.25
             health_regen_timer = 0
     elif health_regen_timer >= 2:
