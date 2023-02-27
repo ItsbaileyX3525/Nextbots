e77bbb2c3cd300e5becbdbbf6abdebe1fb2b8b16
Itsbailey X <baileyleah542@gmail.com>
2023-02-27 17:49:42 +0000

    fixed sound and removed useless stuff

--- multiplayer [WIP]/shared.py
+++
@@ -1,8 +0,0 @@
-from ursina import *
-from ursinanetworking import *
-
-class Ground(Entity, Replicator):
-    def __init__(self, position=(0,0,0)):
-        Entity.__init__(self,scale=1000,parent=scene,position=position,model='cube',origin_y = .5,texture = 'grass',collider='box',)
-        Replicator.__init__(self)
-
